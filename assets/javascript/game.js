fa726853c1c8d31ac96b81125ae1df71a2eaf0e9
Web-dev <webdevmac@users.noreply.github.com>
2018-11-11 14:13:44 -0500

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,9 +11,11 @@ var char9
 var char10
 var char11
 var char12
-var playerHp
-var playerAp
-var counterAp
+var playerHp = 0
+var playerAp = 0
+var counterAp = 0
+var attackValue
+
 
 
 
@@ -26,8 +28,8 @@ $(".img1").click(function() {
 });
 // click the enemy to attack then clears selected enemy and shows them at the defender location
 $(".img2").click(function() {
-    $(".char6, .char7, .char8, #h4-2").hide();
-    $(".char9, .char7, .char8, #h4-1").show();
+    $(".char6, #h4-4").hide();
+    $(".char10, #h4-3").show();
     
 
 });
@@ -37,8 +39,10 @@ $(".img2").click(function() {
 
 // this sets the images to restart the game
 $("#btnReset").click(function () {
+    $(".char1, .char2, .char3, .char4, #h4-3").show();
     $(".char5, .char6, .char7, .char8, .char9, .char10, .char11, .char12, #h4-1, #h4-3").hide();
-    alert("reset works");
+    // attackValue = [Math.floor(Math.random()* 100)]; //calculates Random Number for starting attackvalue 
+    //     alert(attackValue);
 
 });
 
