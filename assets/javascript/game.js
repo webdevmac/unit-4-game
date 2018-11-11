68a1cb7665a4653f95763b466231080e5314a96e
Web-dev <webdevmac@users.noreply.github.com>
2018-11-11 15:59:43 -0500

    updadate

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,81 @@
 
-var char1
-var char2
-var char3
-var char4
-var char5
-var char6
-var char7
-var char8
-var char9
-var char10
-var char11
-var char12
+var character = 0
+var defender = 0
 var playerHp = 0
 var playerAp = 0
 var counterAp = 0
-var attackValue
+var attackValue = 0
 
 
 
 
 //click the character of players choice then clear the other characters when img1 is clicked to select the character
-$(".img1").click(function() {
-    $(".char2, .char3, .char4, #h4-2").hide();
-    $(".char6, .char7, .char8, #h4-1").show();
-    
+if (character == 0) {
+    $(".char1").click(function () {
+        $(".char2, .char3, .char4, #h4-2").hide();
+        $(".char6, .char7, .char8, #h4-1").show();
+        character = 1
 
-});
+    });
+
+    $(".char2").click(function () {
+        $(".char1, .char3, .char4, #h4-2").hide();
+        $(".char5, .char7, .char8, #h4-1").show();
+        character = 1
+    });
+
+    $(".char3").click(function () {
+        $(".char1, .char2, .char4, #h4-2").hide();
+        $(".char5, .char6, .char8, #h4-1").show();
+        character = 1
+
+    });
+    $(".char4").click(function () {
+        $(".char1, .char2, .char3, #h4-2").hide();
+        $(".char5, .char6, .char7, #h4-1").show();
+        character = 1
+
+    });
+}
+else {
+
+}
+
 // click the enemy to attack then clears selected enemy and shows them at the defender location
-$(".img2").click(function() {
-    $(".char6, #h4-4").hide();
-    $(".char10, #h4-3").show();
-    
+if (defender == 0) {
+    $(".char5").click(function () {
+        $(".char5").hide();
+        $(".char9").show();
+        defender = 1
 
-});
+    });
+
+    $(".char6").click(function () {
+        $(".char6").hide();
+        $(".char10").show();
+        defender = 1
+    });
+
+    $(".char7").click(function () {
+        $(".char7").hide();
+        $(".char11").show();
+        defender = 1
+    });
+
+
+    $(".char8").click(function () {
+        $(".char8").hide();
+        $(".char12").show();
+        defender = 1
+
+    });
+}
+else {
+
+}
+
+
+
 
 
 
@@ -40,9 +83,8 @@ $(".img2").click(function() {
 // this sets the images to restart the game
 $("#btnReset").click(function () {
     $(".char1, .char2, .char3, .char4, #h4-3").show();
-    $(".char5, .char6, .char7, .char8, .char9, .char10, .char11, .char12, #h4-1, #h4-3").hide();
-    // attackValue = [Math.floor(Math.random()* 100)]; //calculates Random Number for starting attackvalue 
-    //     alert(attackValue);
+    $(".char5, .char6, .char7, .char8, .char9, .char10, .char11, .char12, #h4-1").hide();
+
 
 });
 
